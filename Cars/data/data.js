9ef46ef655b948d80d6825295db944538be1604f
IVXER <127364002+IVXER@users.noreply.github.com>
2023-10-19 16:23:36 -0500

    Add files via upload

---
+++ Cars/data/data.js
@@ -0,0 +1,33 @@
+export const data= 
+[
+    {
+        "id": 1,
+        "name": "BMW",
+        "image": "/images/1.jpg"
+    },
+    {
+        "id": 2,
+        "name": "Camaro SS",
+        "image": "/images/2.jpg"
+    },
+    {
+        "id": 3,
+        "name": "Ferrari",
+        "image": "/images/3.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Lamborguini",
+        "image": "/images/4.jpg"
+    },
+    {
+        "id": 5,
+        "name": "McLaren",
+        "image": "/images/5.jpg"
+    },
+    {
+        "id": 6,
+        "name": "Porsche",
+        "image": "/images/6.jpg"
+    }
+]
